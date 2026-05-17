bc055cdc1faf3b61d4349aff0d9acb2b9401fc0a
mamargaret <mamargaretagcang20@gmail.com>
2026-05-17 21:03:51 +0800

    modified

--- pages/fiscal_years.php
+++ pages/fiscal_years.php
@@ -1,4 +1,11 @@
-<?php require_once 'sidebar.php'; ?>
+<?php
+require_once '../php/auth_check.php';
+if (!($canApprovePPMP && $canViewReports && $canManageBudget)) {
+  header("Location: 404.php");
+  exit();
+}
+require_once 'sidebar.php';
+?>
 
 <!-- page content -->
 <div class="right_col" role="main">
@@ -32,15 +39,19 @@
                 <div class="modal-body">
                   <div class="form-group">
                     <label for="year">Year</label>
-                    <input type="number" class="form-control" name="year" id="year" placeholder="Enter fiscal year (e.g. 2025)" min="1900" max="2099" oninput="if(this.value.length > 4) this.value = this.value.slice(0,4);" required>
+                    <input type="number" class="form-control" name="year" id="year"
+                      placeholder="Enter fiscal year (e.g. 2025)" min="1900" max="2099"
+                      oninput="if(this.value.length > 4) this.value = this.value.slice(0,4);" required>
                   </div>
                   <div class="form-group">
                     <label for="start_date">Start Date</label>
-                    <input type="date" class="form-control" name="start_date" id="start_date" min="<?= date('Y-m-d') ?>" required>
+                    <input type="date" class="form-control" name="start_date" id="start_date" min="<?= date('Y-m-d') ?>"
+                      required>
                   </div>
                   <div class="form-group">
                     <label for="end_date">End Date</label>
-                    <input type="date" class="form-control" name="end_date" id="end_date" min="<?= date('Y-m-d') ?>" required>
+                    <input type="date" class="form-control" name="end_date" id="end_date" min="<?= date('Y-m-d') ?>"
+                      required>
                   </div>
                 </div>
                 <div class="modal-footer">
@@ -56,15 +67,10 @@
           </div>
         </div>
 
-
         <!-- UPDATE MODAL -->
         <div class="modal fade" id="UpdateModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <form id="UpdateForm" autocomplete="off" enctype="multipart/form-data">
-
-              <input type="hidden" class="form-control" name="user_type" id="edit_user_type" value="Administrator"
-                required>
-
               <div class="modal-content">
                 <div class="modal-header bg-light">
                   <h5 class="modal-title" id="exampleModalLabel">Update Fiscal Year</h5>
@@ -76,28 +82,32 @@
                   <input type="hidden" class="form-control" name="fiscal_year_id" id="edit_fiscal_year_id" required>
                   <div class="form-group">
                     <label for="edit_year">Year</label>
-                    <input type="number" class="form-control" name="year" id="edit_year" placeholder="Enter fiscal year (e.g. 2025)" min="1900" max="2099" oninput="if(this.value.length > 4) this.value = this.value.slice(0,4);" required>
+                    <input type="number" class="form-control" name="year" id="edit_year"
+                      placeholder="Enter fiscal year (e.g. 2025)" min="1900" max="2099"
+                      oninput="if(this.value.length > 4) this.value = this.value.slice(0,4);" required>
                   </div>
                   <div class="form-group">
                     <label for="edit_start_date">Start Date</label>
-                    <input type="date" class="form-control" name="start_date" id="edit_start_date" min="<?= date('Y-m-d') ?>" required>
+                    <input type="date" class="form-control" name="start_date" id="edit_start_date"
+                      min="<?= date('Y-m-d') ?>" required>
                   </div>
                   <div class="form-group">
                     <label for="edit_end_date">End Date</label>
-                    <input type="date" class="form-control" name="end_date" id="edit_end_date" min="<?= date('Y-m-d') ?>" required>
-                  </div>
-                  <div class="form-group">
-                    <label for="edit_is_current">Is Current?</label>
-                    <select class="form-control" name="is_current" id="edit_is_current" required>
-                      <option value="0">No</option>
-                      <option value="1">Yes</option>
-                    </select>
+                    <input type="date" class="form-control" name="end_date" id="edit_end_date"
+                      min="<?= date('Y-m-d') ?>" required>
                   </div>
                   <div class="form-group">
                     <label for="edit_status">Status</label>
                     <select class="form-control" name="status" id="edit_status" required>
                       <option value="1">Active</option>
                       <option value="0">Inactive</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="edit_is_lock">Lock Status</label>
+                    <select class="form-control" name="is_lock" id="edit_is_lock" required>
+                      <option value="0">Open</option>
+                      <option value="1">Locked</option>
                     </select>
                   </div>
                 </div>
@@ -113,50 +123,51 @@
         </div>
 
         <div class="card-box bg-white table-responsive pb-2">
-          <button data-toggle="modal" data-target="#AddNew" class="btn btn-sm btn-primary mb-3 ml-3 mt-1"><i
-              class="fa fa-plus"></i> Create FY</button>
-          <button id="delete-selected" class="btn btn-sm btn-danger mb-3 mt-1"><i class="fa fa-trash"></i>
-            Delete</button>
+          <div class="d-flex justify-content-end mr-2">
+            <button data-toggle="modal" data-target="#AddNew" class="btn btn-sm btn-primary mb-3 ml-3 mt-1"
+              title="Create FY"><i class="fa fa-plus"></i></button>
+            <button id="delete-selected" class="btn btn-sm btn-danger mb-3 mt-1" title="Delete FY"><i
+                class="fa fa-trash"></i></button>
+          </div>
           <table id="datatable" class="table table-striped table-bordered table-hover" style="width:100%">
             <thead>
               <tr class="text-center">
                 <th><input type="checkbox" id="select-all" class="d-block m-auto"></th>
                 <th>YEAR</th>
-                <th>START DATE</th>
-                <th>END DATE</th>
-                <th>IS ACTIVE</th>
+                <th>FISCAL PERIOD</th>
                 <th>STATUS</th>
+                <th>LOCK STATUS</th>
                 <th>ACTIONS</th>
               </tr>
             </thead>
             <tbody>
               <?php
-              $fiscal_years = $db->getAllFiscalYears();
+              $fiscal_years = $db->getFiscalYears();
               while ($row = $fiscal_years->fetch_assoc()) {
                 $status_badge = ($row['status'] == 1)
                   ? '<span class="badge bg-success p-1 rounded text-light">Active</span>'
                   : '<span class="badge bg-secondary p-1 rounded text-light">Inactive</span>';
 
-                $current_badge = ($row['is_current'] == 1)
-                  ? '<span class="badge bg-success p-1 rounded text-light">Current</span>'
-                  : '<span class="badge bg-secondary p-1 rounded text-light">No</span>';
-              ?>
+                $lock_badge = ($row['is_lock'] == 1)
+                  ? '<span class="badge bg-danger p-1 rounded text-light">Locked</span>'
+                  : '<span class="badge bg-info p-1 rounded text-light">Open</span>';
+                ?>
                 <tr class="text-center">
-                  <td><input type="checkbox" class="select-record d-block m-auto" value="<?= $row['fiscal_year_id'] ?>"></td>
+                  <td><input type="checkbox" class="select-record d-block m-auto"
+                      name="record_<?= $row['fiscal_year_id'] ?>" id="record_<?= $row['fiscal_year_id'] ?>"
+                      value="<?= $row['fiscal_year_id'] ?>"></td>
                   <td><?= htmlspecialchars($row['year']); ?></td>
-                  <td><?= date('M. d, Y', strtotime($row['start_date'])); ?></td>
-                  <td><?= date('M. d, Y', strtotime($row['end_date'])); ?></td>
-                  <td><?= $current_badge; ?></td>
+                  <td><?= date('M. d, Y', strtotime($row['start_date'])) .' - '. date('M. d, Y', strtotime($row['end_date'])); ?></td>
                   <td><?= $status_badge; ?></td>
+                  <td><?= $lock_badge; ?></td>
                   <td>
-                    <button 
-                      class="btn btn-success btn-sm edit-fiscalyear" title="Edit Fiscal Year"
+                    <button class="btn btn-success btn-sm edit-fiscalyear" title="Edit Fiscal Year"
                       data-fiscal-year-id="<?= $row['fiscal_year_id']; ?>"
                       data-year="<?= htmlspecialchars($row['year']); ?>"
                       data-start-date="<?= htmlspecialchars($row['start_date']); ?>"
                       data-end-date="<?= htmlspecialchars($row['end_date']); ?>"
-                      data-is-current="<?= htmlspecialchars($row['is_current']); ?>"
-                      data-status="<?= htmlspecialchars($row['status']); ?>">
+                      data-status="<?= htmlspecialchars($row['status']); ?>"
+                      data-is-lock="<?= htmlspecialchars($row['is_lock']); ?>">
                       <i class="fa fa-edit"></i>
                     </button>
                   </td>
@@ -178,7 +189,7 @@
     $('#SaveForm').submit(function (e) {
       e.preventDefault();
       var formData = new FormData($(this)[0]);
-      formData.append('action', 'AddFiscalYearForm');
+      formData.append('action', 'createFiscalYear');
       $.ajax({
         type: 'POST',
         url: '../php/processes.php',
@@ -197,31 +208,29 @@
       });
     });
 
-    
-    // Populate update modal with candidate data
+
     $('#datatable').on('click', '.edit-fiscalyear', function () {
       const fiscal_year_id = $(this).data('fiscal-year-id');
       const year = $(this).data('year');
       const start_date = $(this).data('start-date');
       const end_date = $(this).data('end-date');
-      const is_current = $(this).data('is-current');
       const status = $(this).data('status');
+      const is_lock = $(this).data('is-lock');
 
       $('#edit_fiscal_year_id').val(fiscal_year_id);
       $('#edit_year').val(year);
       $('#edit_start_date').val(start_date);
       $('#edit_end_date').val(end_date);
-      $('#edit_is_current').val(is_current);
       $('#edit_status').val(status);
+      $('#edit_is_lock').val(is_lock);
 
-      // Show modal
       $('#UpdateModal').modal('show');
     });
 
     $('#UpdateForm').submit(function (e) {
       e.preventDefault();
       var formData = new FormData($(this)[0]);
-      formData.append('action', 'UpdateFiscalYearForm');
+      formData.append('action', 'updateFiscalYear');
       $.ajax({
         type: 'POST',
         url: '../php/processes.php',
@@ -240,24 +249,20 @@
       });
     });
 
-    // MULTIPLE DELETION
     $('#select-all').on('click', function () {
       var isChecked = $(this).is(':checked');
       $('.select-record').prop('checked', isChecked);
     });
 
     $('#datatable').on('change', '.select-record', function () {
-      // If any checkbox is unchecked, uncheck "select-all"
       if (!$(this).is(':checked')) {
         $('#select-all').prop('checked', false);
       } else {
-        // If all checkboxes are checked, check the "select-all"
         var allChecked = $('.select-record').length === $('.select-record:checked').length;
         $('#select-all').prop('checked', allChecked);
       }
     });
 
-    // Delete selected button
     $('#delete-selected').on('click', function () {
       var selectedIDs = [];
       $('.select-record:checked').each(function () {
@@ -270,10 +275,9 @@
       }
 
       confirmMultipleDeletion(selectedIDs.length, function () {
-        deleteMultipleRecords("users", "user_id", selectedIDs, "users.php");
+        deleteMultipleRecords("fiscal_years", "fiscal_year_id", selectedIDs, "fiscal_years.php");
       });
     });
-
 
   });
 
